403e11c79c42340c45fd1465e4bb2d414f95b19f
Imanol-Acosta <ilacosta05@hotmail.com>
2026-03-04 18:30:07 -0400

    initial commit

---
+++ app/src/main/java/ucne/edu/imanol_acosta_ap2_p2/MainActivity.kt
@@ -0,0 +1,47 @@
+package ucne.edu.imanol_acosta_ap2_p2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ucne.edu.imanol_acosta_ap2_p2.ui.theme.Imanol_Acosta_AP2_P2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Imanol_Acosta_AP2_P2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Imanol_Acosta_AP2_P2Theme {
+        Greeting("Android")
+    }
+}
